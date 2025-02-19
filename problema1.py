87075bdc5ad98a826cb243170aa200a6ff09093f
Fabrizio Solf <78766598+MKFabrizio@users.noreply.github.com>
2025-02-18 22:17:13 -0500

    Update problema1.py

--- problema1.py
+++ problema1.py
@@ -2,7 +2,29 @@
 
 def main():
     S       = input()
+    def es_palindrome(palabra:str):
+        if len(palabra)%2==1:
+            medio = int(np.floor((len(palabra))/2))
+            parte_1 = palabra[:medio]
+            parte_2 = palabra[medio+1:][::-1]
+            if parte_1==parte_2:
+                print('Es palindrome')
+            else:
+                print('No es palindrome')
+        else:
+            medio_1 = int(np.floor((len(palabra))/2)) - 1
+            medio_2 = int(np.floor((len(palabra))/2))
     
+            if palabra[medio_1]==palabra[medio_2]:
+                parte_1 = palabra[:medio_1]
+                parte_2 = palabra[medio_2+1:][::-1]
+                if parte_1==parte_2:
+                    print('Es palindrome')
+                else:
+                    print('No es palindrome')
+            else:
+                print('No es palindrome')
+    es_palindrome(S)
 
 if __name__ == '__main__':
     main()
